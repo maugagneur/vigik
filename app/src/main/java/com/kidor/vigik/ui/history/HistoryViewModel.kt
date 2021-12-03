6ad4fb2fc70bfa8e584731383ed94f08449e5567
Maurin Augagneur <maurin.augagneur@viveris.fr>
2021-12-03 10:48:16 +0100

    No need to use MediatorLiveData for history ViewModel

--- app/src/main/java/com/kidor/vigik/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/kidor/vigik/ui/history/HistoryViewModel.kt
@@ -1,10 +1,14 @@
 package com.kidor.vigik.ui.history
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.kidor.vigik.db.TagRepository
 import com.kidor.vigik.nfc.model.Tag
 import com.kidor.vigik.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,22 +17,18 @@ class HistoryViewModel @Inject constructor(
     private val repository: TagRepository
 ) : BaseViewModel<HistoryViewState, Nothing>() {
 
-    override val _viewState: MediatorLiveData<HistoryViewState> = MediatorLiveData<HistoryViewState>()
-
-    init {
-        val initialViewState: MutableLiveData<HistoryViewState> = MutableLiveData<HistoryViewState>()
-        initialViewState.value = HistoryViewState.Initializing
-
-        _viewState.addSource(initialViewState) { _viewState.value = it }
-        _viewState.addSource(repository.allTags.asLiveData()) { tags ->
-            _viewState.removeSource(initialViewState)
-            _viewState.value = if (tags.isEmpty()) {
+    override val viewState: LiveData<HistoryViewState> = repository.allTags
+        .map { tags ->
+            if (tags.isEmpty()) {
                 HistoryViewState.NoTag
             } else {
                 HistoryViewState.DisplayTags(tags.sortedByDescending { it.timestamp })
             }
         }
-    }
+        .onStart {
+            emit(HistoryViewState.Initializing)
+        }
+        .asLiveData()
 
     fun deleteTag(tag: Tag) {
         viewModelScope.launch {
