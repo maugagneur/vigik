fae0a44efeca320bdaff7fe872fce7ca34bb45c0
Maurin Augagneur <maurin.augagneur@viveris.fr>
2022-05-16 10:46:22 +0200

    Migrate History screen and associated tests to Compose

--- app/src/androidTest/java/com/kidor/vigik/ui/history/HistoryViewTest.kt
+++ app/src/androidTest/java/com/kidor/vigik/ui/history/HistoryViewTest.kt
@@ -1,21 +1,30 @@
 package com.kidor.vigik.ui.history
 
+import androidx.compose.ui.semantics.ProgressBarRangeInfo
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertRangeInfoEquals
+import androidx.compose.ui.test.filter
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kidor.vigik.R
-import com.kidor.vigik.db.TagRepository
-import com.kidor.vigik.extensions.launchFragmentInHiltContainer
+import com.kidor.vigik.extensions.onNodeWithText
 import com.kidor.vigik.nfc.model.Tag
-import com.kidor.vigik.utils.EspressoUtils.checkViewIsNotVisible
-import com.kidor.vigik.utils.EspressoUtils.checkViewIsVisible
 import com.kidor.vigik.utils.TestUtils.logTestName
-import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import java.util.Calendar
 
 /**
  * Integration tests for [HistoryFragment].
@@ -25,62 +34,119 @@ import javax.inject.Inject
 class HistoryViewTest {
 
     @get:Rule
-    var hiltRule = HiltAndroidRule(this)
+    val composeTestRule = createComposeRule()
 
-    @Inject
-    lateinit var repository: TagRepository
+    private lateinit var closeable: AutoCloseable
+
+    @Mock
+    private lateinit var viewActionCallback: (HistoryViewAction) -> Unit
 
     @Before
     fun setUp() {
-        hiltRule.inject()
+        closeable = MockitoAnnotations.openMocks(this)
+    }
+
+    @After
+    fun tearDown() {
+        closeable.close()
     }
 
     @Test
-    fun checkUiElementsWhenInitializing() {
+    fun checkLoadingState() {
         logTestName()
 
-        // Load fragment in empty fragment activity and force state `Initializing`
-        launchFragmentInHiltContainer<HistoryFragment> { fragment ->
-            fragment.stateRender(HistoryViewState.Initializing)
+        composeTestRule.setContent {
+            LoadingState()
         }
 
-        // Check that the recyclerview of tags is hidden
-        checkViewIsNotVisible(R.id.tag_history_recyclerview, "History RecyclerView")
+        // Check that loader is visible
+        composeTestRule
+            .onNodeWithTag(PROGRESS_BAR_TEST_TAG)
+            .assertIsDisplayed()
+            .assertRangeInfoEquals(ProgressBarRangeInfo.Indeterminate)
 
-        // Check that the textview for `no-data` is hidden
-        checkViewIsNotVisible(R.id.no_data_textview, "No-data TextView")
+        // Check that the list of tags is hidden
+        composeTestRule
+            .onNodeWithTag(TAGS_LIST_TEST_TAG)
+            .assertDoesNotExist()
+
+        // Check that the text for `no-data` is hidden
+        composeTestRule
+            .onNodeWithText(stringResourceId = R.string.no_data_label)
+            .assertDoesNotExist()
     }
 
     @Test
-    fun checkUiElementsAtStartWhenThereIsNoTagFromDatabase() {
+    fun checkUiElementsWhenThereIsNoTag() {
         logTestName()
 
-        // Load fragment in empty fragment activity
-        launchFragmentInHiltContainer<HistoryFragment>()
-
-        // Check that the recyclerview of tags is hidden
-        checkViewIsNotVisible(R.id.tag_history_recyclerview, "History RecyclerView")
-
-        // Check that the textview for `no-data` is hidden
-        checkViewIsVisible(R.id.no_data_textview, "No-data TextView")
-    }
-
-    @Test
-    fun checkUiElementsAtStartWhenThereIsTagsFromDatabase() {
-        logTestName()
-
-        // Insert empty tag in database before launching screen
-        runBlocking {
-            repository.insert(Tag())
+        composeTestRule.setContent {
+            DisplayTags(DisplayTagsStateData(emptyList()))
         }
 
-        // Load fragment in empty fragment activity
-        launchFragmentInHiltContainer<HistoryFragment>()
+        // Check that loader is not visible
+        composeTestRule
+            .onNodeWithTag(PROGRESS_BAR_TEST_TAG)
+            .assertDoesNotExist()
 
-        // Check that the recyclerview of tags is hidden
-        checkViewIsVisible(R.id.tag_history_recyclerview, "History RecyclerView")
+        // Check that the list of tags is hidden
+        composeTestRule
+            .onNodeWithTag(TAGS_LIST_TEST_TAG)
+            .assertDoesNotExist()
 
-        // Check that the textview for `no-data` is hidden
-        checkViewIsNotVisible(R.id.no_data_textview, "No-data TextView")
+        // Check that the textview for `no-data` is visible
+        composeTestRule
+            .onNodeWithText(stringResourceId = R.string.no_data_label)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun checkUiElementsWhenThereIsTags() {
+        logTestName()
+
+        val firstTag = Tag()
+        val secondTag = Tag(timestamp = 42)
+        val now = Calendar.getInstance().timeInMillis
+        val thirdTag = Tag(timestamp = now)
+        val tagList = listOf(firstTag, secondTag, thirdTag)
+
+        composeTestRule.setContent {
+            DisplayTags(DisplayTagsStateData(
+                tagList,
+                viewActionCallback
+            ))
+        }
+
+        // Check that loader is not visible
+        composeTestRule
+            .onNodeWithTag(PROGRESS_BAR_TEST_TAG)
+            .assertDoesNotExist()
+
+        // Check that the list of tags is visible with all elements
+        composeTestRule
+            .onNodeWithTag(TAGS_LIST_TEST_TAG)
+            .assertIsDisplayed()
+            .onChildren()
+            .filter(hasTestTag(TAGS_LIST_ROW_TEST_TAG))
+            .assertCountEquals(tagList.size)
+
+        // Check that the textview for `no-data` is visible
+        composeTestRule
+            .onNodeWithText(stringResourceId = R.string.no_data_label)
+            .assertDoesNotExist()
+
+        // Check that a click on delete button generates a DeleteTag action with right value
+        composeTestRule
+            .onNodeWithTag(DELETE_ICON_TEST_TAG_PREFIX + 0)
+            .performClick()
+        verify(viewActionCallback).invoke(HistoryViewAction.DeleteTag(firstTag))
+        composeTestRule
+            .onNodeWithTag(DELETE_ICON_TEST_TAG_PREFIX + 42)
+            .performClick()
+        verify(viewActionCallback).invoke(HistoryViewAction.DeleteTag(secondTag))
+        composeTestRule
+            .onNodeWithTag(DELETE_ICON_TEST_TAG_PREFIX + now)
+            .performClick()
+        verify(viewActionCallback).invoke(HistoryViewAction.DeleteTag(thirdTag))
     }
 }
--- app/src/main/java/com/kidor/vigik/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/kidor/vigik/ui/history/HistoryFragment.kt
@@ -1,64 +1,125 @@
 package com.kidor.vigik.ui.history
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.kidor.vigik.databinding.FragmentHistoryBinding
-import com.kidor.vigik.nfc.model.Tag
+import com.kidor.vigik.R
+import com.kidor.vigik.extensions.toHex
 import com.kidor.vigik.ui.base.BaseFragment
-import com.kidor.vigik.ui.history.list.HistoryAdapter
-import com.kidor.vigik.ui.history.list.TagHistoryListener
+import com.kidor.vigik.ui.compose.AppTheme
+import com.kidor.vigik.ui.usecases.FormatDateUseCase
 import dagger.hilt.android.AndroidEntryPoint
+
+internal const val DELETE_ICON_TEST_TAG_PREFIX = "Delete tag "
+internal const val TAGS_LIST_TEST_TAG = "Tags list"
+internal const val TAGS_LIST_ROW_TEST_TAG = "Tags list row"
+internal const val PROGRESS_BAR_TEST_TAG = "Progress bar"
 
 /**
  * View that display all tags saved in the database.
  */
 @AndroidEntryPoint
-class HistoryFragment : BaseFragment<HistoryViewAction, HistoryViewState, Nothing, HistoryViewModel>(),
-    TagHistoryListener {
+class HistoryFragment : BaseFragment<HistoryViewAction, HistoryViewState, Nothing, HistoryViewModel>() {
 
     override val viewModel by viewModels<HistoryViewModel>()
 
-    override val isComposeView: Boolean = false
+    override val isComposeView: Boolean = true
 
-    private lateinit var binding: FragmentHistoryBinding
-    private lateinit var historyAdapter: HistoryAdapter
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        historyAdapter = HistoryAdapter(this)
-        binding = FragmentHistoryBinding.inflate(inflater, container, false).also {
-            it.tagHistoryRecyclerview.let { recyclerView ->
-                recyclerView.adapter = historyAdapter
-                recyclerView.layoutManager = LinearLayoutManager(context)
-            }
-        }
-        return binding.root
-    }
-
-    override fun stateRender(viewState: HistoryViewState) {
+    @Composable
+    override fun StateRender(viewState: HistoryViewState) {
         when (viewState) {
-            HistoryViewState.Initializing -> {
-                binding.tagHistoryRecyclerview.visibility = View.GONE
-                binding.noDataTextview.visibility = View.GONE
-                historyAdapter.submitList(emptyList())
-            }
-            is HistoryViewState.DisplayTags -> {
-                binding.tagHistoryRecyclerview.visibility = View.VISIBLE
-                binding.noDataTextview.visibility = View.GONE
-                historyAdapter.submitList(viewState.tags)
-            }
-            HistoryViewState.NoTag -> {
-                binding.tagHistoryRecyclerview.visibility = View.GONE
-                binding.noDataTextview.visibility = View.VISIBLE
-                historyAdapter.submitList(emptyList())
-            }
+            HistoryViewState.Initializing -> LoadingState()
+            is HistoryViewState.DisplayTags ->
+                DisplayTags(DisplayTagsStateData(viewState.tags) { action -> viewModel.handleAction(action) })
+            HistoryViewState.NoTag -> DisplayTags(DisplayTagsStateData(emptyList()))
         }
     }
+}
 
-    override fun onDeleteTagClick(tag: Tag) {
-        viewModel.handleAction(HistoryViewAction.DeleteTag(tag))
+@Composable
+@Preview(widthDp = 400, heightDp = 700)
+@VisibleForTesting
+internal fun LoadingState() {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.testTag(PROGRESS_BAR_TEST_TAG),
+            color = AppTheme.colors.secondary
+        )
+    }
+}
+
+@Composable
+@Preview(widthDp = 400, heightDp = 700)
+@VisibleForTesting
+internal fun DisplayTags(@PreviewParameter(DisplayTagsStateProvider::class) displayTagsStateData: DisplayTagsStateData) {
+    if (displayTagsStateData.tags.isEmpty()) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.no_data_label),
+                fontSize = AppTheme.dimensions.textSizeMedium
+            )
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .padding(AppTheme.dimensions.commonSpaceSmall)
+                .testTag(TAGS_LIST_TEST_TAG)
+        ) {
+            items(displayTagsStateData.tags) { tag ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .testTag(TAGS_LIST_ROW_TEST_TAG),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = FormatDateUseCase().invoke(tag.timestamp),
+                        modifier = Modifier.weight(0.5f),
+                        fontSize = AppTheme.dimensions.textSizeMedium,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = tag.uid?.toHex() ?: "",
+                        modifier = Modifier.weight(0.3f),
+                        fontSize = AppTheme.dimensions.textSizeMedium,
+                        textAlign = TextAlign.Center
+                    )
+                    IconButton(
+                        onClick = { displayTagsStateData.onViewAction(HistoryViewAction.DeleteTag(tag)) },
+                        modifier = Modifier
+                            .weight(0.2f)
+                            .testTag(DELETE_ICON_TEST_TAG_PREFIX + (tag.timestamp ?: 0))
+                    ) {
+                        Icon(
+                            Icons.Default.Delete,
+                            contentDescription = "Delete tag"
+                        )
+                    }
+                }
+            }
+        }
     }
 }
