474c02360ec4b9506ca4fbfef37a7c64a6cbb270
Maurin Augagneur <maurin.augagneur@viveris.fr>
2022-08-10 17:00:20 +0200

    Add a menu item to switch application's theme in the future

--- app/src/main/java/com/kidor/vigik/ui/MainActivity.kt
+++ app/src/main/java/com/kidor/vigik/ui/MainActivity.kt
@@ -3,8 +3,13 @@ package com.kidor.vigik.ui
 import android.content.Intent
 import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +17,7 @@ import androidx.navigation.ui.NavigationUI
 import com.kidor.vigik.R
 import com.kidor.vigik.databinding.ActivityMainBinding
 import com.kidor.vigik.nfc.api.NfcApi
+import com.kidor.vigik.ui.compose.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -40,6 +46,22 @@ class MainActivity : AppCompatActivity() {
                 appBarConfiguration = AppBarConfiguration.Builder(it.graph).build()
                 NavigationUI.setupActionBarWithNavController(this, it)
             }
+
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return if (menuItem.itemId == R.id.action_stop_invert_color) {
+                    AppTheme.invertTheme()
+                    Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+                    true
+                } else {
+                    false
+                }
+            }
+        })
     }
 
     override fun onResume() {
