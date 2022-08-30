3e768fab9322046c016b41e4782f7c175b2850da
Maurin Augagneur <maurin.augagneur@viveris.fr>
2022-08-30 17:14:34 +0200

    Simplify Check screen startup

--- app/src/main/java/com/kidor/vigik/ui/check/CheckViewModel.kt
+++ app/src/main/java/com/kidor/vigik/ui/check/CheckViewModel.kt
@@ -18,9 +18,6 @@ class CheckViewModel @Inject constructor(
     private val nfcApi: NfcApi
 ) : BaseViewModel<CheckViewAction, CheckViewState, CheckViewEvent>() {
 
-    // This flag indicates if the first NFC check has been done.
-    private var firstNfcCheckDone: Boolean = false
-
     init {
         _viewState.value = CheckViewState.Loading
     }
@@ -37,15 +34,10 @@ class CheckViewModel @Inject constructor(
     }
 
     private fun performNfcCheck() {
-        if (firstNfcCheckDone) {
-            _viewState.value = CheckViewState.Loading
-            viewModelScope.launch {
-                // Add a delay for all no-first refresh request to force the UI to reflect this transition state
-                delay(TIME_BEFORE_NFC_CHECK)
-                checkIfNfcIsAvailable()
-            }
-        } else {
-            // The first NFC check is done right at the launch of the screen so it does not need a delay.
+        _viewState.value = CheckViewState.Loading
+        viewModelScope.launch {
+            // Add a delay for all no-first refresh request to force the UI to reflect this transition state
+            delay(TIME_BEFORE_NFC_CHECK)
             checkIfNfcIsAvailable()
         }
     }
@@ -58,6 +50,5 @@ class CheckViewModel @Inject constructor(
         } else {
             _viewState.value = CheckViewState.NfcIsDisable
         }
-        firstNfcCheckDone = true
     }
 }
