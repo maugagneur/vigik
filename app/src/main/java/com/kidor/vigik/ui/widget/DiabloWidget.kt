8ca3e15ed8794a289f12bb308cc8c575803b462f
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-07-04 17:28:35 +0200

    Open the app when clicking on widget

--- app/src/main/java/com/kidor/vigik/ui/widget/DiabloWidget.kt
+++ app/src/main/java/com/kidor/vigik/ui/widget/DiabloWidget.kt
@@ -2,11 +2,24 @@ package com.kidor.vigik.ui.widget
 
 import android.content.Context
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.fillMaxSize
+import com.kidor.vigik.ui.MainActivity
 /**
  * Diablo IV event tracker widget.
  */
 class DiabloWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            Box(
+                modifier = GlanceModifier.fillMaxSize()
+                    .clickable(onClick = actionStartActivity(activity = MainActivity::class.java)),
+            ) {
+            }
+        }
 }
