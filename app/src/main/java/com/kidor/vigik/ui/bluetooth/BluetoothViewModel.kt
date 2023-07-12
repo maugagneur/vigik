634327bb96d24a03091e431ef3673b6791efa6c5
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-07-12 17:16:55 +0200

    Do not listen Bluetooth and Location states on main thread

--- app/src/main/java/com/kidor/vigik/ui/bluetooth/BluetoothViewModel.kt
+++ app/src/main/java/com/kidor/vigik/ui/bluetooth/BluetoothViewModel.kt
@@ -2,8 +2,10 @@ package com.kidor.vigik.ui.bluetooth
 
 import androidx.lifecycle.viewModelScope
 import com.kidor.vigik.data.bluetooth.BluetoothApi
+import com.kidor.vigik.di.IoDispatcher
 import com.kidor.vigik.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,16 +14,17 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class BluetoothViewModel @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
     private val bluetoothApi: BluetoothApi
 ) : BaseViewModel<BluetoothViewAction, BluetoothViewState, Nothing>() {
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(ioDispatcher) {
             bluetoothApi.bluetoothEnable.collect { bluetoothEnable ->
                 updateViewState { it.copy(isBluetoothEnable = bluetoothEnable) }
             }
         }
-        viewModelScope.launch {
+        viewModelScope.launch(ioDispatcher) {
             bluetoothApi.locationEnable.collect { locationEnable ->
                 updateViewState { it.copy(isLocationEnable = locationEnable) }
             }
@@ -34,6 +37,6 @@ class BluetoothViewModel @Inject constructor(
      * @param update The operation to perform on view state.
      */
     private fun updateViewState(update: (BluetoothViewState) -> BluetoothViewState) {
-        _viewState.value = update(viewState.value ?: BluetoothViewState())
+        _viewState.postValue(update(viewState.value ?: BluetoothViewState()))
     }
 }
