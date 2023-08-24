6366d38dbc899ad37ea1f3b64da76fa7e1c0a613
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-08-24 09:24:11 +0200

    Update dependencies
    Upgrade Compose Compiler version from 1.5.1 to 1.5.3
    Upgrade Kotlin version from 1.9.0 to 1.9.10
    Upgrade KSP version from 1.9.0-1.0.12 to 1.9.10-1.0.13
    
    Fix missing Kotlin plugin declaration for convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.diktat) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.ksp) apply false
 }
 
