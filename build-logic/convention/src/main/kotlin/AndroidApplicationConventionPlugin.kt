ba108da7a028219a8efc8788531917252106d8d8
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-10-27 15:42:22 +0200

    Update app version to 2.8.0

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -16,8 +16,8 @@ private const val TARGET_SDK_VER = 34
 private const val MIN_SDK_VER = 23
 
 private const val APP_MAJOR_VERSION = 2
-private const val APP_MINOR_VERSION = 7
-private const val APP_HOTFIX_VERSION = 2
+private const val APP_MINOR_VERSION = 8
+private const val APP_HOTFIX_VERSION = 0
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
