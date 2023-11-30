4a93fc08c702e95e4236766a1c5d99b3daf25908
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-11-30 09:39:03 +0100

    Fix wrong text color on "NFC emulate tag" screen

--- app/src/main/java/com/kidor/vigik/ui/nfc/emulate/EmulateScreen.kt
+++ app/src/main/java/com/kidor/vigik/ui/nfc/emulate/EmulateScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,6 +41,7 @@ internal fun DisplayLogLineState(@PreviewParameter(LogProvider::class) logLines:
         Text(
             text = logLines,
             modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.onBackground,
             fontSize = AppTheme.dimensions.textSizeMedium
         )
     }
