ea95e1ff4670dfa300ab706a8e7d607adb28e3a1
Maurin Augagneur <maurin.augagneur@viveris.fr>
2023-12-01 16:38:37 +0100

    Create empty screen for bottom sheet feature

---
+++ app/src/main/java/com/kidor/vigik/ui/bottomsheet/BottomSheetScreen.kt
@@ -0,0 +1,14 @@
+package com.kidor.vigik.ui.bottomsheet
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * View that display the section dedicated to the custom bottom sheet.
+ */
+@Composable
+fun BottomSheetScreen() {
+    Box(modifier = Modifier.fillMaxSize())
+}
