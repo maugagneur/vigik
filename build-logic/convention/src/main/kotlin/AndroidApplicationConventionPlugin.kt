9df8b38ac1aabf265fe17c4b8ecdfe57172c0b72
Maurin Augagneur <maurin.augagneur@viveris.fr>
2024-06-17 09:53:42 +0200

    Upgrade Kotlin version to 2.0.0

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -8,7 +8,7 @@ import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 private const val COMPILE_SDK_VER = 34
@@ -96,26 +96,20 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
             tasks.withType<KotlinCompile>().configureEach {
-                kotlinOptions {
-                    // Set JVM target to 17
-                    jvmTarget = JavaVersion.VERSION_17.toString()
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
                     // Treat all Kotlin warnings as errors (disabled by default)
                     // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
                     val warningsAsErrors: String? by project
-                    allWarningsAsErrors = warningsAsErrors.toBoolean()
-                    freeCompilerArgs = freeCompilerArgs + listOf(
-                        "-opt-in=kotlin.RequiresOptIn",
-                        // Enable experimental coroutines APIs, including Flow
-                        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-                        "-opt-in=kotlinx.coroutines.FlowPreview",
+                    allWarningsAsErrors.set(warningsAsErrors.toBoolean())
+                    optIn.set(
+                        listOf(
+                            "-opt-in=kotlin.RequiresOptIn",
+                            // Enable experimental coroutines APIs, including Flow
+                            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                            "-opt-in=kotlinx.coroutines.FlowPreview",
+                        )
                     )
-                }
-            }
-
-            // Use Kotlin K2 compiler
-            kotlinExtension.sourceSets.all {
-                languageSettings {
-                    languageVersion = "2.0"
                 }
             }
 
