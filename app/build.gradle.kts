7fdbcea91b4aa53e0f25ed15b3e3d7e513f26d83
Maurin Augagneur <maurin.augagneur@viveris.fr>
2024-11-14 16:47:56 +0100

    Add Google Maps dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     // Log
     implementation(libs.timber)
 
+    // Map
+    implementation(libs.maps.compose)
+
     // Network calls
     implementation(libs.bundles.retrofit)
     implementation(libs.conscrypt)
