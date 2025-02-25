44d29efd29ce8aedb850c243e28b054abc5a89ce
Maurin Augagneur <maurin.augagneur@viveris.fr>
2025-02-25 17:26:43 +0100

    Update app version to 2.22.0

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -16,7 +16,7 @@ private const val TARGET_SDK_VER = 35
 private const val MIN_SDK_VER = 23
 
 private const val APP_MAJOR_VERSION = 2
-private const val APP_MINOR_VERSION = 21
+private const val APP_MINOR_VERSION = 22
 private const val APP_FIX_VERSION = 0
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
