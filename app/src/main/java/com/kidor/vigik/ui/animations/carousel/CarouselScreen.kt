8ad8a08dbb272bab88981ad80a0c0ca698a3bff1
Maurin Augagneur <maurin.augagneur@viveris.fr>
2025-03-24 17:05:58 +0100

    Clean code

--- app/src/main/java/com/kidor/vigik/ui/animations/carousel/CarouselScreen.kt
+++ app/src/main/java/com/kidor/vigik/ui/animations/carousel/CarouselScreen.kt
@@ -42,9 +42,11 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.GraphicsLayerScope
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
@@ -72,9 +74,12 @@ private val IMAGES = listOf(
     R.drawable.notification_big_picture,
     R.drawable.road
 )
-private const val PARALLAX_COEFFICIENT = 0.4f
+private const val CIRCLE_PARALLAX_COEFFICIENT = 0.4f
 private const val RIGHT_ANGLE = 90f
 private const val CUBE_VISUAL_ANGLE = 105f // Having an angle upper than 90° make a better perspective render
+private const val BLUR_COEFFICIENT = 20f
+private const val MINIMUM_BLUR_RADIUS = 0.1f
+private const val DECAL_PARALLAX_COEFFICIENT = 0.2f
 
 /**
  * View that display the section dedicated to the custom carousel animation.
@@ -126,13 +131,30 @@ fun CarouselScreen(
     }
 }
 
+/**
+ * A customizable carousel.
+ *
+ * @param modifier   The modifier to be applied to the carousel.
+ * @param mode       The mode of the carousel.
+ * @param pagerState The state to control this carousel.
+ */
 @OptIn(ExperimentalComposeUiApi::class)
+@Suppress("LongMethod")
 @Composable
 private fun Carousel(
     modifier: Modifier = Modifier,
     mode: CarouselMode,
     pagerState: PagerState
 ) {
+    val contentPadding = if (mode == CarouselMode.ZOOM) {
+        PaddingValues(horizontal = MaterialTheme.dimensions.commonSpaceXLarge)
+    } else {
+        PaddingValues(all = 0.dp)
+    }
+    val beyondViewportPageCount = if (mode == CarouselMode.DECAL) 2 else PagerDefaults.BeyondViewportPageCount
+    val pageSpacing = if (mode == CarouselMode.ZOOM) MaterialTheme.dimensions.commonSpaceMedium else 0.dp
+    val endPadding = if (mode == CarouselMode.DECAL) MaterialTheme.dimensions.commonSpaceXLarge else 0.dp
+
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center
@@ -140,6 +162,7 @@ private fun Carousel(
         // Blur effect is only supported on Android 12 and above
         if (mode == CarouselMode.CUBE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val offsetFromStart = pagerState.offsetForPage(0).absoluteValue
+            // Add a shadow under the carousel
             Box(
                 modifier = Modifier
                     .aspectRatio(1f)
@@ -155,13 +178,6 @@ private fun Carousel(
         }
 
         var offsetY by remember { mutableFloatStateOf(0f) }
-        val contentPadding = if (mode == CarouselMode.ZOOM) {
-            PaddingValues(horizontal = MaterialTheme.dimensions.commonSpaceXLarge)
-        } else {
-            PaddingValues(all = 0.dp)
-        }
-        val beyondViewportPageCount = if (mode == CarouselMode.DECAL) 2 else PagerDefaults.BeyondViewportPageCount
-        val pageSpacing = if (mode == CarouselMode.ZOOM) MaterialTheme.dimensions.commonSpaceMedium else 0.dp
 
         HorizontalPager(
             state = pagerState,
@@ -182,104 +198,145 @@ private fun Carousel(
                 imageLoader = SingletonImageLoader.get(LocalContext.current),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .apply {
-                        if (mode == CarouselMode.DECAL) {
-                            zIndex(page * 10f)
-                            padding(
-                                start = MaterialTheme.dimensions.commonSpaceXLarge * 2,
-                                end = MaterialTheme.dimensions.commonSpaceXLarge
-                            )
-                        }
-                    }
+                    .zIndex(page.toFloat())
+                    .padding(
+                        start = endPadding * 2,
+                        end = endPadding
+                    )
                     .graphicsLayer {
-                        when (mode) {
-                            CarouselMode.ZOOM -> {
-                                val offset = pagerState.offsetForPage(page).absoluteValue
-                                lerp(
-                                    start = 0.75f,
-                                    stop = 1f,
-                                    fraction = 1f - offset.coerceIn(0f, 1f)
-                                ).let { scale ->
-                                    scaleY = scale
-                                }
-                            }
-
-                            CarouselMode.CUBE -> {
-                                val offset = pagerState.offsetForPage(page)
-                                val isOnTheRight = offset < 0f
-                                val degrees = CUBE_VISUAL_ANGLE
-                                val interpolated = FastOutLinearInEasing.transform(offset.absoluteValue)
-                                rotationY = min(interpolated * if (isOnTheRight) degrees else -degrees, RIGHT_ANGLE)
-
-                                transformOrigin = TransformOrigin(
-                                    pivotFractionX = if (isOnTheRight) 0f else 1f,
-                                    pivotFractionY = 0.5f
-                                )
-                            }
-
-                            CarouselMode.CIRCLE_REVEAL -> {
-                                // Make the left page not move
-                                val offset = pagerState.offsetForPage(page)
-                                translationX = size.width * offset
-
-                                // Add a circular clipping shape on right page
-                                val endOffset = pagerState.endOffsetForPage(page)
-                                shape = CirclePath(
-                                    progress = 1f - endOffset.absoluteValue,
-                                    origin = Offset(
-                                        x = size.width,
-                                        y = offsetY
-                                    )
-                                )
-                                clip = true
-
-                                // Add a parallax effect
-                                val scale = 1f + (offset.absoluteValue * PARALLAX_COEFFICIENT)
-                                scaleX = scale
-                                scaleY = scale
-
-                                // Fade the left page away
-                                val startOffset = pagerState.startOffsetForPage(page)
-                                alpha = (2f - startOffset) / 2f
-                            }
-
-                            CarouselMode.DECAL -> {
-                                // Make the left page not move
-                                val startOffset = pagerState.startOffsetForPage(page)
-                                translationX = size.width * startOffset
-
-                                // Fade the left page away
-                                alpha = (2f - startOffset) / 2f
-
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                                    val blur = (startOffset * 20f).coerceAtLeast(0.1f)
-                                    renderEffect = RenderEffect
-                                        .createBlurEffect(blur, blur, Shader.TileMode.DECAL)
-                                        .asComposeRenderEffect()
-                                }
-
-                                // Add a parallax effect on the left page
-                                val scale = 1f - (startOffset * 0.2f)
-                                scaleX = scale
-                                scaleY = scale
-                            }
-
-                            else -> { /* Do nothing */
-                            }
-                        }
+                        customizeGraphicLayer(
+                            scope = this,
+                            mode = mode,
+                            pagerState = pagerState,
+                            page = page,
+                            offsetY = offsetY
+                        )
                     }
                     .drawWithContent {
-                        if (mode == CarouselMode.CUBE) {
-                            // Dim the cube face as it turns away from the center
-                            val offset = pagerState.offsetForPage(page)
-                            drawContent()
-                            drawRect(color = Color.Black.copy(alpha = offset.absoluteValue * 0.7f))
-                        } else {
-                            drawContent()
-                        }
+                        customizeDrawWithContent(
+                            scope = this,
+                            mode = mode,
+                            pagerState = pagerState,
+                            page = page
+                        )
                     }
             )
         }
+    }
+}
+
+/**
+ * Customization of the graphics layer for each of carousel modes.
+ *
+ * @param scope      The scope which can be used to define the effects to apply for the content.
+ * @param mode       The mode of the carousel.
+ * @param pagerState The state to control the carousel.
+ * @param page       The current page of the carousel.
+ */
+private fun customizeGraphicLayer(
+    scope: GraphicsLayerScope,
+    mode: CarouselMode,
+    pagerState: PagerState,
+    page: Int,
+    offsetY: Float
+) {
+    when (mode) {
+        CarouselMode.ZOOM -> {
+            val offset = pagerState.offsetForPage(page).absoluteValue
+            lerp(
+                start = 0.75f,
+                stop = 1f,
+                fraction = 1f - offset.coerceIn(0f, 1f)
+            ).let { scale ->
+                scope.scaleY = scale
+            }
+        }
+
+        CarouselMode.CUBE -> {
+            val offset = pagerState.offsetForPage(page)
+            val isOnTheRight = offset < 0f
+            val degrees = CUBE_VISUAL_ANGLE
+            val interpolated = FastOutLinearInEasing.transform(offset.absoluteValue)
+            scope.rotationY = min(interpolated * if (isOnTheRight) degrees else -degrees, RIGHT_ANGLE)
+
+            scope.transformOrigin = TransformOrigin(
+                pivotFractionX = if (isOnTheRight) 0f else 1f,
+                pivotFractionY = 0.5f
+            )
+        }
+
+        CarouselMode.CIRCLE_REVEAL -> {
+            // Make the left page not move
+            val offset = pagerState.offsetForPage(page)
+            scope.translationX = scope.size.width * offset
+
+            // Add a circular clipping shape on right page
+            val endOffset = pagerState.endOffsetForPage(page)
+            scope.shape = CirclePath(
+                progress = 1f - endOffset.absoluteValue,
+                origin = Offset(
+                    x = scope.size.width,
+                    y = offsetY
+                )
+            )
+            scope.clip = true
+
+            // Add a parallax effect
+            val scale = 1f + (offset.absoluteValue * CIRCLE_PARALLAX_COEFFICIENT)
+            scope.scaleX = scale
+            scope.scaleY = scale
+
+            // Fade the left page away
+            val startOffset = pagerState.startOffsetForPage(page)
+            scope.alpha = (2f - startOffset) / 2f
+        }
+
+        CarouselMode.DECAL -> {
+            // Make the left page not move
+            val startOffset = pagerState.startOffsetForPage(page)
+            scope.translationX = scope.size.width * startOffset
+
+            // Fade the left page away
+            scope.alpha = (2f - startOffset) / 2f
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val blur = (startOffset * BLUR_COEFFICIENT).coerceAtLeast(MINIMUM_BLUR_RADIUS)
+                scope.renderEffect = RenderEffect
+                    .createBlurEffect(blur, blur, Shader.TileMode.DECAL)
+                    .asComposeRenderEffect()
+            }
+
+            // Add a parallax effect on the left page
+            val scale = 1f - (startOffset * DECAL_PARALLAX_COEFFICIENT)
+            scope.scaleX = scale
+            scope.scaleY = scale
+        }
+
+        else -> { /* Do nothing */ }
+    }
+}
+
+/**
+ * Customization of the layout's content depending of the carousel mode.
+ *
+ * @param scope      The scope for drawing content.
+ * @param mode       The mode of the carousel.
+ * @param pagerState The state to control the carousel.
+ * @param page       The current page of the carousel.
+ */
+private fun customizeDrawWithContent(
+    scope: ContentDrawScope,
+    mode: CarouselMode,
+    pagerState: PagerState,
+    page: Int
+) {
+    if (mode == CarouselMode.CUBE) {
+        // Dim the cube face as it turns away from the center
+        val offset = pagerState.offsetForPage(page)
+        scope.drawContent()
+        scope.drawRect(color = Color.Black.copy(alpha = offset.absoluteValue * 0.7f))
+    } else {
+        scope.drawContent()
     }
 }
 
